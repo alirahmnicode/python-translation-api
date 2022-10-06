0d4ddd8d72b66a6f19a5270b1b501f55561a56ee
alirahmanicode <alirahmanicode@gmail.com>
2022-10-06 09:22:40 +0330

    create base translate class

--- translator/google.py
+++ translator/google.py
@@ -1,24 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+from .translate import BaseTranslate
+from .constant import BASE_URL
 
-base_url = "https://translate.google.com/m"
-params = {
-    "sl": "en",
-    "tl": "fa",
-    "q": "how are you"
-}
 
-class GoogleTranslate:
-    def __init__(self, text: str):
-        self.text = text
-        self.element_tag="div",
-        self.element_query={"class": "t0"},
+    
+class GoogleTranslate(BaseTranslate):
+    """
+    use google for translate
+    """
+    def __init__(self, text: str, target: str, source: str):
+        self.element_tag = "div",
+        self.element_query = {"class": "t0"},
         self._alt_element_query = {"class": "result-container"}
+        super().__init__(text, target, source)
 
     def translate(self):
-        r = requests.get(url=base_url, params=params)
+        r = requests.get(url=BASE_URL, params=self.get_params())
         s = BeautifulSoup(r.text, 'html.parser')
         element = s.find(self.element_tag, self.element_query)
         if not element:
                 element = s.find(self.element_tag, self._alt_element_query)
         return element.get_text(strip=True)
+
+    def get_params(self):
+        params = dict()
+        params['tl'] = self.target
+        params['sl'] = self.source
+        params['q'] = self.text
+        return params
---
+++ translator/translate.py
@@ -0,0 +1,18 @@
+from .exceptions import LanguageNotSupportedException
+from .constant import GOOGLE_LANGUAGES_TO_CODES as gls
+
+
+class BaseTranslate:
+    def __init__(self, text: str, target: str, source: str):
+        self.text = text
+        self.source = source
+        self._check_language_support(target=target)
+
+    def _check_language_support(self, target: str):
+        """
+        check if target language supported or not
+        """
+        if target in gls.keys() or target in gls.values():
+            self.target = target
+        else:
+            raise LanguageNotSupportedException()
