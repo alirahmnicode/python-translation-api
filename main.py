35c8879bc2148f1de58979f1f96c86146472935d
alirahmanicode <alirahmanicode@gmail.com>
2022-10-07 17:05:20 +0330

    add languages support endpoint

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from translator.google import GoogleTranslate
+from translator.constant import GOOGLE_LANGUAGES_TO_CODES
 
 
 app = FastAPI()
 
 
-@app.get("/{text}/")
-def translate(text: str):
-    gt = GoogleTranslate(text)
-    s = gt.translate()
-    return s
+@app.get("/translate/")
+def translate(text: str, tl: str, sl: str):
+    gt = GoogleTranslate(text=text, target=tl, source=sl)
+    translated = gt.translate()
+    return translated
+
+
+@app.get("/languages/support/")
+def languages_support():
+    return GOOGLE_LANGUAGES_TO_CODES
